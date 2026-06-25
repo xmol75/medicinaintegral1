eef5099fdf60a1c90ea3bfa0d4b0ed3109331d76
xmol75 <xmol@outlook.com>
2026-06-25 13:51:22 +0200

    Add Firebase web config

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDHtN5QYhf3LeTH1elaR1sLYGzTLGNUBmY",
+  authDomain: "medicinaintegral1-49e20.firebaseapp.com",
+  projectId: "medicinaintegral1-49e20",
+  storageBucket: "medicinaintegral1-49e20.firebasestorage.app",
+  messagingSenderId: "954610924548",
+  appId: "1:954610924548:web:bf0f0d6b2e242a8557c951",
+  measurementId: "G-437SXWT4FE"
+};
+
+export const caseDocumentPath = "teacherCases/caso1";
